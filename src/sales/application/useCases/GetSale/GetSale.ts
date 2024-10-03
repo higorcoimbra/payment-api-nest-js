091e0e185b2b9ea265a502e93cd5871dcd87e47e
Higor Coimbra <higor1308@gmail.com>
2024-10-03 14:50:45 -0300

    refactor: removes entity from get sale usecase

--- src/sales/application/useCases/GetSale/GetSale.ts
+++ src/sales/application/useCases/GetSale/GetSale.ts
@@ -1,11 +1,36 @@
-import { Sale } from '../../../domain/entity/Sale';
 import SaleRepository from '../../../infrastructure/repositories/SaleRepository';
 
 export default class GetSale {
   constructor(private readonly saleRepository: SaleRepository) {}
 
-  async execute(id: string): Promise<Sale> {
-    const sale = this.saleRepository.findSaleById(id);
-    return sale;
+  async execute(id: string): Promise<Output> {
+    const sale = await this.saleRepository.findSaleById(id);
+    const outputItems = [];
+    for (const item of sale.items) {
+      outputItems.push({
+        id: item.id,
+        productId: item.productId,
+        quantity: item.quantity,
+        price: item.price,
+      });
+    }
+    return {
+      id: sale.id,
+      salesmanId: sale.salesmanId,
+      date: sale.date,
+      items: outputItems,
+    };
   }
 }
+
+type Output = {
+  id: string;
+  salesmanId: string;
+  date: Date;
+  items: {
+    id: string;
+    productId: string;
+    quantity: number;
+    price: number;
+  }[];
+};
